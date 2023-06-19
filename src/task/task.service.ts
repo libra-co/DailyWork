597db234a42b2610b6b74d027e2bc7edc7aad6de
liuhongbo <liuhongbo@dip-ai.com>
2023-06-19 10:05:12 +0800

    feat: tes

--- src/task/task.service.ts
+++ src/task/task.service.ts
@@ -2,7 +2,7 @@
  * @Author: liuhongbo liuhongbo@dip-ai.com
  * @Date: 2023-06-14 15:53:15
  * @LastEditors: liuhongbo liuhongbo@dip-ai.com
- * @LastEditTime: 2023-06-14 19:10:47
+ * @LastEditTime: 2023-06-15 09:30:10
  * @FilePath: /DailyWork/src/task/task.service.ts
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */

904ecc716e49435cd905c83359eb68207685b9b0
liuhongbo <916196375@qq.com>
2023-07-02 20:53:19 +0800

    feat: get pci

--- src/scheduletask/const.ts
+++ src/scheduletask/const.ts
@@ -1,3 +1,25 @@
+import exp from "constants"
+
 // 请求该地址,活期摸鱼报图片url
 export const toushfishingUrl = 'https://api.vvhan.com/api/moyu?type=json'
-export const tinyPngApiKey = 'Wk79GlJr6CSzWMPd8FrnV5KPqjtnStmc'
+export const shrinkPnkUrl = 'https://api.tinify.com/shrink'
+export const tinyPngApiKey = 'VwzhN6SLWyz8pK2y2jWDcZM0hXFLMDNy'
+
+export interface TinyPngRes {
+    input: Input;
+    output: Output;
+}
+
+interface Output {
+    size: number;
+    type: string;
+    width: number;
+    height: number;
+    ratio: number;
+    url: string;
+}
+
+interface Input {
+    size: number;
+    type: string;
+}

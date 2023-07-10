ba77aee80130f2efea0463bcac78d4aa94970d49
liuhongbo <liuhongbo@dip-ai.com>
2023-07-10 18:33:55 +0800

    feat: schedule

--- src/scheduletask/const.ts
+++ src/scheduletask/const.ts
@@ -1,25 +1,6 @@
-import exp from "constants"
-
-// 请求该地址,活期摸鱼报图片url
+// 请求该地址,获取摸鱼报图片url
 export const toushfishingUrl = 'https://api.vvhan.com/api/moyu?type=json'
+// tinypng api url
 export const shrinkPnkUrl = 'https://api.tinify.com/shrink'
+// tinypng api key
 export const tinyPngApiKey = 'VwzhN6SLWyz8pK2y2jWDcZM0hXFLMDNy'
-
-export interface TinyPngRes {
-    input: Input;
-    output: Output;
-}
-
-interface Output {
-    size: number;
-    type: string;
-    width: number;
-    height: number;
-    ratio: number;
-    url: string;
-}
-
-interface Input {
-    size: number;
-    type: string;
-}
--- src/scheduletask/scheduletask.service.ts
+++ src/scheduletask/scheduletask.service.ts
@@ -1,8 +1,8 @@
 /*
  * @Author: liuhongbo 916196375@qq.com
  * @Date: 2023-07-01 22:49:53
- * @LastEditors: liuhongbo 916196375@qq.com
- * @LastEditTime: 2023-07-02 20:50:09
+ * @LastEditors: liuhongbo liuhongbo@dip-ai.com
+ * @LastEditTime: 2023-07-03 14:11:08
  * @FilePath: \daily-work\src\scheduleTask\scheduleTask.service.ts
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -10,7 +10,9 @@ import { Injectable } from '@nestjs/common';
 import axios from 'axios';
 import { RedisCacheProvider } from 'src/redis/redis.cache.provider';
 import { Cron, CronExpression } from '@nestjs/schedule';
-import { TinyPngRes, shrinkPnkUrl, tinyPngApiKey } from './const';
+import { tinyPngApiKey } from './const';
+const tinify = require("tinify");
+tinify.key = tinyPngApiKey;
 
 @Injectable()
 export class ScheduleTaskService {
@@ -21,35 +23,28 @@ export class ScheduleTaskService {
         try {
             const currentImgCode = await this.redis.get('loginImage')
             if (currentImgCode) return null
-
             // 请求图片地址
             const { data: { url: imgUrl } } = await axios.get('https://api.vvhan.com/api/moyu?type=json')
-            // 请求tinypng,压缩图片
-            const { output: { url: shrinkImgUrl } } = await axios.post(shrinkPnkUrl,
-                {
-                    "source": {
-                        "url": imgUrl
-                    },
-                },
-                {
-                    headers: {
-                        Authorization: `Basic ${Buffer.from(`api:${tinyPngApiKey}`).toString("base64")}`,
-                        "Content-Type": "application/json",
-                    },
-                },
-            ) as TinyPngRes
-
             // 请求图片,转为base64
-            const res = await axios.get(shrinkImgUrl, {
+            const res = await axios.get(imgUrl, {
                 responseType: 'arraybuffer'
             })
-            const imgBuffer = Buffer.from(res.data, 'binary').toString('base64')
-            // 设置图片过期时间
-            const currentTimestamp = Math.floor(Date.now() / 1000);
-            const secondsInDay = 24 * 60 * 60;
-            const secondsUntilEndOfDay = secondsInDay - (currentTimestamp % secondsInDay);
-            await this.deleteLoginImg()
-            await this.redis.setWithExpiration('loginImage', imgBuffer, secondsUntilEndOfDay)
+            // 将压缩图片放入Redis
+            const storeImgBufferInRedis = async (imgBuffer: Buffer) => {
+                const currentTimestamp = Math.floor(Date.now() / 1000);
+                const secondsInDay = 24 * 60 * 60;
+                const secondsUntilEndOfDay = secondsInDay - (currentTimestamp % secondsInDay);
+                await this.deleteLoginImg()
+                await this.redis.setWithExpiration('loginImage', imgBuffer.toString('base64'), secondsUntilEndOfDay)
+            }
+            // 请求tinypng地址压缩图片
+            await tinify.fromBuffer(Buffer.from(res.data), 'binary').toBuffer(async function (err, resultData) {
+                if (err) throw err;
+                await tinify.fromBuffer(Buffer.from(resultData), 'binary').toBuffer(async function (err, resultData) {
+                    if (err) throw err;
+                    storeImgBufferInRedis(resultData)
+                })
+            })
 
         } catch (error) {
             console.log('获取图片出错', error)

2dad77901d12dc8e81ae33a67f48b0a6180f244f
liuhongbo <liuhongbo@dip-ai.com>
2023-08-31 14:55:30 +0800

    feat: update imports in project

--- src/project/project.module.ts
+++ src/project/project.module.ts
@@ -2,7 +2,7 @@
  * @Author: liuhongbo liuhongbo@dip-ai.com
  * @Date: 2023-06-12 17:52:54
  * @LastEditors: liuhongbo liuhongbo@dip-ai.com
- * @LastEditTime: 2023-06-12 17:53:39
+ * @LastEditTime: 2023-08-01 19:08:35
  * @FilePath: /DailyWork/src/projectMangement/projectmangement.module.ts
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -13,8 +13,10 @@ import { PrismaService } from 'src/prisima.service';
 import { TaskService } from 'src/task/task.service';
 import { ColumnService } from 'src/column/column.service';
 import { NotionService } from 'src/notion/notion.service';
+import { TaskModule } from 'src/task/task.module';
 
 @Module({
+  imports:[TaskModule],
   controllers: [ProjectmangementController],
   providers: [ProjectmangementService, PrismaService,TaskService,ColumnService,NotionService]
 })

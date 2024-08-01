d3068d8e072c784429930359e28ba7b96ed278b0
liuhongbo <916196375@qq.com>
2024-08-02 00:30:07 +0800

    feat: change env

--- src/redis/redis.config.ts
+++ src/redis/redis.config.ts
@@ -7,10 +7,13 @@
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
 import * as IORedis from 'ioredis';
+import { env } from 'process';
+
+console.log('env.REDIS_HOST', env.REDIS_HOST);
 
 export const redisConfig: IORedis.RedisOptions = {
-  host: '139.155.5.202', // Redis 服务器地址
+  host: env.REDIS_HOST, // Redis 主机地址
   port: 6379, // Redis 端口号
-  password: '1391555202', // Redis 访问密码，若无密码请移除此行
+  // password: '1391555202', // Redis 访问密码，若无密码请移除此行
   db: 0, // Redis 数据库编号
 };
